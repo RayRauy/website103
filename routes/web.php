68a20d522ef452197e9108f7a94473746378f199
RayRau <ngosoyhout1423@gmaile.com>
2025-08-24 14:17:32 +0700

    Added the Menu table and MVC

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -21,5 +22,10 @@ Route::get('role-edit/{id}', [RoleController::class, 'edit']) -> name('role.edit
 Route::put('role-update/{id}', [RoleController::class, 'update']) -> name('role.update');
 Route::get('role-destroy/{id}', [RoleController::class, 'destroy']) -> name('role.delete');
 
-
+Route::get('menu-index', [MenuController::class, 'index']) -> name('menu.index');
+Route::get('menu-create', [MenuController::class, 'create']) -> name('menu.create');
+Route::post('menu-store', [MenuController::class, 'store']) -> name('menu.store');
+Route::get('menu-edit/{id}', [MenuController::class, 'edit']) -> name('menu.edit');
+Route::put('menu-update/{id}', [MenuController::class, 'update']) -> name('menu.update');
+Route::get('menu-destroy/{id}', [MenuController::class, 'destroy']) -> name('menu.delete');
 // Route::resource('roles', 'RoleController');
