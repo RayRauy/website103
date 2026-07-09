f9ce2d08b8afc0b9fabd47c8e706ffcde6d2ae9c
RayRau <ngosoyhout1423@gmaile.com>
2026-07-09 23:23:24 +0700

    More Modification to Proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,8 +6,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
+        $middleware->trustProxies(at: '*');
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ use \Illuminate\Http\Request;
 
 Route::get('/', action: function () {
     return view('home');
-}) -> name('index');
+})->name('index');
 Auth::routes();
 
 //Optionals: Save Theme on reload
-Route::post('/theme-set', function(Request $request){
+Route::post('/theme-set', function (Request $request) {
     $request->validate([
         'theme' => 'required|in:light,dark'
     ]);
@@ -32,53 +32,60 @@ Route::post('/theme-set', function(Request $request){
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //Roles
-Route::middleware(['permission:view roles'])->get('role-index', [RoleController::class, 'index']) -> name('role.index');
-Route::get('role-create', [RoleController::class, 'create']) -> name('role.create');
-Route::post('role-store', [RoleController::class, 'store']) -> name('role.store');
-Route::get('role-edit/{id}', [RoleController::class, 'edit']) -> name('role.edit');
-Route::put('role-update/{id}', [RoleController::class, 'update']) -> name('role.update');
-Route::get('role-destroy/{id}', [RoleController::class, 'destroy']) -> name('role.delete');
+Route::middleware(['permission:view roles'])->get('role-index', [RoleController::class, 'index'])->name('role.index');
+Route::get('role-create', [RoleController::class, 'create'])->name('role.create');
+Route::post('role-store', [RoleController::class, 'store'])->name('role.store');
+Route::get('role-edit/{id}', [RoleController::class, 'edit'])->name('role.edit');
+Route::put('role-update/{id}', [RoleController::class, 'update'])->name('role.update');
+Route::get('role-destroy/{id}', [RoleController::class, 'destroy'])->name('role.delete');
 
 //Menus
-Route::get('menu-index', [MenuController::class, 'index']) -> name('menu.index');
-Route::get('menu-create', [MenuController::class, 'create']) -> name('menu.create');
-Route::post('menu-store', [MenuController::class, 'store']) -> name('menu.store');
-Route::get('menu-edit/{id}', [MenuController::class, 'edit']) -> name('menu.edit');
-Route::put('menu-update/{id}', [MenuController::class, 'update']) -> name('menu.update');
-Route::get('menu-destroy/{id}', [MenuController::class, 'destroy']) -> name('menu.delete');
+Route::get('menu-index', [MenuController::class, 'index'])->name('menu.index');
+Route::get('menu-create', [MenuController::class, 'create'])->name('menu.create');
+Route::post('menu-store', [MenuController::class, 'store'])->name('menu.store');
+Route::get('menu-edit/{id}', [MenuController::class, 'edit'])->name('menu.edit');
+Route::put('menu-update/{id}', [MenuController::class, 'update'])->name('menu.update');
+Route::get('menu-destroy/{id}', [MenuController::class, 'destroy'])->name('menu.delete');
 
 //Site Settings
-Route::get('sites-index', [SiteSettingController::class, 'index']) -> name('site.index');
-Route::get('sites-create', [SiteSettingController::class, 'create']) -> name('site.create');
-Route::post('sites-store', [SiteSettingController::class, 'store']) -> name('site.store');
-Route::get('sites-edit/{id}', [SiteSettingController::class, 'edit']) -> name('site.edit');
-Route::put('sites-update/{id}', [SiteSettingController::class, 'update']) -> name('site.update');
-Route::get('sites-destroy/{id}', [SiteSettingController::class, 'destroy']) -> name('site.delete');
+Route::get('sites-index', [SiteSettingController::class, 'index'])->name('site.index');
+Route::get('sites-create', [SiteSettingController::class, 'create'])->name('site.create');
+Route::post('sites-store', [SiteSettingController::class, 'store'])->name('site.store');
+Route::get('sites-edit/{id}', [SiteSettingController::class, 'edit'])->name('site.edit');
+Route::put('sites-update/{id}', [SiteSettingController::class, 'update'])->name('site.update');
+Route::get('sites-destroy/{id}', [SiteSettingController::class, 'destroy'])->name('site.delete');
 
 //Posts
-Route::get('posts-index', [PostController::class, 'index']) -> name('post.index');
-Route::get('posts-create', [PostController::class, 'create']) -> name('post.create');
-Route::post('posts-store', [PostController::class, 'store']) -> name('post.store');
-Route::get('posts-edit/{id}', [PostController::class, 'edit']) -> name('post.edit');
-Route::put('posts-update/{id}', [PostController::class, 'update']) -> name('post.update');
-Route::delete('posts-destroy/{id}', [PostController::class, 'destroy']) -> name('post.delete');
+Route::get('posts-index', [PostController::class, 'index'])->name('post.index');
+Route::get('posts-create', [PostController::class, 'create'])->name('post.create');
+Route::post('posts-store', [PostController::class, 'store'])->name('post.store');
+Route::get('posts-edit/{id}', [PostController::class, 'edit'])->name('post.edit');
+Route::put('posts-update/{id}', [PostController::class, 'update'])->name('post.update');
+Route::delete('posts-destroy/{id}', [PostController::class, 'destroy'])->name('post.delete');
 
 //Banners
-Route::get('banners-index', [BannerController::class, 'index']) -> name('banner.index');
-Route::get('banners-create', [BannerController::class, 'create']) -> name('banner.create');
-Route::post('banners-store', [BannerController::class, 'store']) -> name('banner.store');
-Route::get('banners-edit/{id}', [BannerController::class, 'edit']) -> name('banner.edit');
-Route::put('banners-update/{id}', [BannerController::class, 'update']) -> name('banner.update');
-Route::get('banners-destroy/{id}', [BannerController::class, 'destroy']) -> name('banner.delete');
+Route::get('banners-index', [BannerController::class, 'index'])->name('banner.index');
+Route::get('banners-create', [BannerController::class, 'create'])->name('banner.create');
+Route::post('banners-store', [BannerController::class, 'store'])->name('banner.store');
+Route::get('banners-edit/{id}', [BannerController::class, 'edit'])->name('banner.edit');
+Route::put('banners-update/{id}', [BannerController::class, 'update'])->name('banner.update');
+Route::get('banners-destroy/{id}', [BannerController::class, 'destroy'])->name('banner.delete');
 
 //Users
-Route::middleware(['permission:view users'])->get('users-index', [UserController::class, 'index']) -> name('user.index');
-Route::middleware(['permission:create users'])->get('users-create', [UserController::class, 'create']) -> name('user.create');
-Route::middleware(['permission:create users'])->post('users-store', [UserController::class, 'store']) -> name('user.store');
-Route::middleware(['permission:edit users'])->get('users-edit/{id}', [UserController::class, 'edit']) -> name('user.edit');
-Route::middleware(['permission:edit users'])->put('users-update/{id}', [UserController::class, 'update']) -> name('user.update');
-Route::middleware(['permission:delete users'])->get('users-destroy/{id}', [UserController::class, 'destroy']) -> name('user.delete');
+Route::middleware(['permission:view users'])->get('users-index', [UserController::class, 'index'])->name('user.index');
+Route::middleware(['permission:create users'])->get('users-create', [UserController::class, 'create'])->name('user.create');
+Route::middleware(['permission:create users'])->post('users-store', [UserController::class, 'store'])->name('user.store');
+Route::middleware(['permission:edit users'])->get('users-edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+Route::middleware(['permission:edit users'])->put('users-update/{id}', [UserController::class, 'update'])->name('user.update');
+Route::middleware(['permission:delete users'])->get('users-destroy/{id}', [UserController::class, 'destroy'])->name('user.delete');
 // Route::resource('roles', 'RoleController');
 
 //Test test
-Route::get('/test-print', [PrinterController::class, 'printReceipt']) -> name('test.print');
+Route::get('/test-print', [PrinterController::class, 'printReceipt'])->name('test.print');
+Route::get('/debug-ip', function () {
+    return [
+        'ip' => request()->ip(),
+        'remote' => request()->server('REMOTE_ADDR'),
+        'headers' => request()->headers->all(),
+    ];
+});
