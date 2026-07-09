58320b77723646a95549473227a35434f6e080ab
RayRau <ngosoyhout1423@gmaile.com>
2026-07-09 23:13:36 +0700

    Modified the TrustProxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,4 +1,6 @@
 protected $proxies = '*';
-protected $headers = Request::HEADER_X_FORWARDED_FOR |
-                      Request::HEADER_X_FORWARDED_HOST |
-                      Request::HEADER_X_FORWARDED_PROTO;
+protected $headers =
+Request::HEADER_X_FORWARDED_FOR |
+Request::HEADER_X_FORWARDED_HOST |
+Request::HEADER_X_FORWARDED_PORT |
+Request::HEADER_X_FORWARDED_PROTO;
